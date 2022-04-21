fe36bf3aec96c0373b5a20bfd6e19fdf6b8e4ae2
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-20 20:48:13 -0500

    Se agrega la clase User

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,4 +1,4 @@
-const User =  require("./../app/User")
+const User =  require("./../../app/models/User")
 
 describe("Unit Test for User class", () => {
     test('create a User Object', () => {
@@ -7,11 +7,11 @@ describe("Unit Test for User class", () => {
 
         //Aqui validas los resultados de esta codigo
         //Esta es una comparacion que va a igualar el valor de la izquiera con el valor de la derecha (valor esperado)
-        expext(user.id).toBe(1)
-        expext(user.userName).toBe("carlogilmar")
-        expext(user.name).toBe("Carlo")
-        expext(user.bio).toBe("Bio")
-        expext(user.dateCreated).toBe("dateCreated")
-        expext(user.lastUpdated).toBe("lastUpdated")
+        expect(user.id).toBe(1)
+        expect(user.userName).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
     });
 })
