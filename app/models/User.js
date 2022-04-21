d7ead860223f5e959f8d0f4af7ba0ac88c09b634
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-20 20:51:35 -0500

    Se agrega el constructor en la clase User

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,12 @@
 class User{
-
+    constructor(id, userName, name, bio, dateCreated, lastUpdated){
+        this.id = id
+        this.userName = userName
+        this.name = name
+        this.bio = bio
+        this.dateCreated = dateCreated
+        this.lastUpdated = lastUpdated
+    }
 }
 
 module.exports = User
