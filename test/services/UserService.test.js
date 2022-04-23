e49810cbc3aab997babbc68e4c1a96e0191d056a
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 18:27:40 -0500

    se agrega codigo a la clase userSercice.js

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,7 +3,7 @@ const UserService = require('./../../app/services/UserService')
 describe("Test for UserService", () => {
     test("Tests for UserService", () => {
         const user = UserService.create(1, "carlogilmar", "Carlo")
-        expect(user.Username).toBe("carlogilmar")
+        expect(user.userName).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
