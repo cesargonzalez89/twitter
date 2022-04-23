fd5ffe3ccc72d0115d69684723f684f4ec6165e6
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 18:44:56 -0500

    Se agrega codigo para el metodo getInfo en la clase UserService con lo cual la prueba pasa

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,11 @@ class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+    
+    static getInfo(user = new User()){
+        return [user.id, user.userName, user.name, user.bio]
+    }
+
 }
 
 module.exports = UserService
