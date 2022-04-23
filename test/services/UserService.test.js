e735d61fb88fefbdfab9bae99043e7ac8c27c434
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 18:49:17 -0500

    Se agrega prueba para el metodo updateUsername la cual  falla

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -17,4 +17,10 @@ describe("Test for UserService", () => {
         expect(userInfoList[2]).toBe("Carlo")
         expect(userInfoList[3]).toBe("Sin bio")
     })
+
+    test("3. Update username", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo")
+        UserService.updateUsername(user, "carlog")
+        expect(user.userName).toBe("carlog")
+    })
 })
