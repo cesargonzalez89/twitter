ef95c3be693d2c82529c44224ec2d474dee1299d
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 20:13:01 -0500

    Se agrega el codigo en la clase UserView para validar los nulos en las llaves de payload, la prueba unitaria pasa

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -3,7 +3,11 @@ const userService = require('./../models/User')
 class UserView{
     static createUser(payload){
         if(payload!=null){
+            if(payload.username!=null && payload.name!=null && payload.id!=null){
 
+            }else{
+                return{error: "necesitan tener un valor valido"}
+            }
         }else{
             return{error: "payload no existe"}
         }
