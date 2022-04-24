f97fe7af9ec0cbe8876f7d067d4bcbf56c8cbe4d
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 19:47:04 -0500

    Se agrega el repositorio para views con la primera prueba para validar un error

---
+++ test/views/UserViews.test.js
@@ -0,0 +1,10 @@
+const UserView = require('./../views/UserView')
+
+describe("Test for UserView", () => {
+    test("Return an error object when try to create new user with an null payload", () => {
+        const payload = null
+        const result = UserView.createUser(payload)
+        // https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/payload no existe/)
+    })
+})
