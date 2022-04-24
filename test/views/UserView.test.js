b050ffcb41499e1f7dfa5cb41f3fd3ed2acbd824
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-23 20:19:19 -0500

    Se agrega la prueba y codigo para validad que todas las propiedades de payload esten presentes

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -8,8 +8,14 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/payload no existe/)
     })
 
-    test("Retur an error object when try to create a new user with a payload with invalid properties", () => {
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test("Return an error object when try to create a new user with a missing properties", () => {
+        const payload = {username: "Username"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
